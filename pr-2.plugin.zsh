e1dd75cf686e41250985e26ad95d700e71143cd3
Grigorii Horos <horosgrisa@gmail.com>
2019-07-21 14:17:13 +0300

    Fix deps

--- pr-2.plugin.zsh
+++ pr-2.plugin.zsh
@@ -1,3 +1,8 @@
 #!/usr/bin/env zsh
+DEPENDENCES_ZSH+=( zpm-zsh/colors )
 
-pr_2="%{$fg_bold[green]%}%_>%{$reset_color%}"
+if command -v zpm >/dev/null; then
+  zpm zpm-zsh/colors
+fi
+
+pr_2="%{$c[green]$c_bold%}%_>%{$c_reset%}"

51e1c42d56da4413dafd5daa6effc67b727c858b
Grigorii Horos <horosgrisa@gmail.com>
2020-03-29 03:20:57 +0300

    Optimize

--- pr-2.plugin.zsh
+++ pr-2.plugin.zsh
@@ -1,7 +1,3 @@
 #!/usr/bin/env zsh
 
-if (( $+functions[zpm] )); then #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
-  zpm zpm-zsh/colors #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
-fi #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
-
 typeset -g pr_2="%{$c[green]$c_bold%}%_>%{$c_reset%}"
